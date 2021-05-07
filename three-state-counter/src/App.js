8e299efeb21ff8ea848eb969a2db88afb3cbe692
Parth Parikh <parthparikh03@gmail.com>
2021-05-07 16:23:57 +0530

    three-state-counter
    i have made a simple counter app with use of react state and leanr how state is actually implemented in reactjs

---
+++ three-state-counter/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Counter App Using State/hooks </h1>
+      </header>
+      <h2>Current Value Of Count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => (count >= 10 ? "" : setCount(count + 1))}>
+        Increase Counter
+      </button>
+      <button onClick={() => (count > 0 ? setCount(count - 1) : "")}>
+        Decrease Counter
+      </button>
+    </div>
+  );
+}
+
+export default App;

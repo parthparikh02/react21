c61439377108adc937c2e0c277deed18ea18c376
Parth Parikh <parthparikh03@gmail.com>
2021-05-12 12:50:43 +0530

    mygithubapp
    made an application with firebase authnetication to fetch user data from github API

---
+++ ten-firebase-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyB8zzWrgeFtSfSeDL3Xpww_zFTNDbZ3XPM",
+  authDomain: "mygitapp-db237.firebaseapp.com",
+  projectId: "mygitapp-db237",
+  storageBucket: "mygitapp-db237.appspot.com",
+  messagingSenderId: "582939196509",
+  appId: "1:582939196509:web:fe06493418128872cc61b4",
+  measurementId: "G-RD7KWG7F3N",
+};
